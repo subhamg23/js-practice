dbffffb3ee02e21b00e796f2cf6ddda929f3841a
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-18 11:00:18 +0000

    code config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12344
+let accountMail = "subham@hotmail.com"
+var accountPassword = "12323"
+accountCity = "Kolkata"
+
+console.log(accountId)
+console.table([accountId,accountMail,accountPassword,accountCity])
