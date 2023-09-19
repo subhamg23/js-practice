dce266878b24bdb11ee3f253b89e4e812d897eb9
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-19 11:12:03 +0000

    function basic

---
+++ 03_basics_func/01_functions.js
@@ -0,0 +1,13 @@
+
+function addTwoNumbers(num1,num2){
+    return num1+num2
+}
+
+const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
+
+function userLoginMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(userLoginMessage("Subham"));
