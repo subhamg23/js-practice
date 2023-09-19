dfb0320fdc8bba590cd3a0e1642906dd6c817462
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-19 06:50:11 +0000

    array operations

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,8 @@
+const marvel = ["Ironman", "Captain America", "Dr Strange"]
+const dc = ["Superman", "Flash", "Batman"]
+
+// marvel.push(dc)
+console.log(marvel);
+
+const allHeros = marvel.concat(dc) //concatination stores the data in a different array
+console.log(allHeros);
