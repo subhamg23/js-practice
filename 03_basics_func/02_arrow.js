bb5ae7f807ab507e0a2d46f071db771fcccf2454
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-19 14:36:46 +0000

    basic arrow functions

--- 03_basics_func/02_arrow.js
+++ 03_basics_func/02_arrow.js
@@ -6,6 +6,13 @@ const user = {
     }
 }
 
-console.log(user.welcomeMessage());
+// console.log(user.welcomeMessage());
 user.username = "Subham"
-console.log(user.welcomeMessage());
+// console.log(user.welcomeMessage());
+
+const addTwoNumbers = (n1,n2) => {
+    return n1+n2
+}
+const addTwoNumbers2 = (n1,n2) => n1+n2 //implicit return
+
+console.log(addTwoNumbers2(56,7));
