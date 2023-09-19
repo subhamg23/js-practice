c2d3f87ae871571a8b51e027446e19c1439d0c9a
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-19 06:13:47 +0000

    date time feature

---
+++ 01_basics/datesinJs.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2023,7,23)
+console.log(myCreatedDate.toDateString())
+
