569b74775c7bc59d18641ddad416f87142652631
subhamg23 <84767865+subhamg23@users.noreply.github.com>
2023-09-19 12:22:10 +0530

    Delete 02_ArrayPart_1 directory

--- 02_ArrayPart_1/02_arrays.js
+++
@@ -1,8 +0,0 @@
-const marvel = ["Ironman", "Captain America", "Dr Strange"]
-const dc = ["Superman", "Flash", "Batman"]
-
-// marvel.push(dc)
-console.log(marvel);
-
-const allHeros = marvel.concat(dc) //concatination stores the data in a different array
-console.log(allHeros);
